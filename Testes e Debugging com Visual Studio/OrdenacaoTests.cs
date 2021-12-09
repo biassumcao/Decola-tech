83d67d03ddb91df5c40e8c2546866857701ea6b9
Beatriz Assumção da Silva <beatriz.assumcao@unifesp.br>
2021-12-09 16:57:53 -0300

    Adicionando programa com metodos de testes

---
+++ Testes e Debugging com Visual Studio/OrdenacaoTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ordenacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordenacao.Tests
+{
+    [TestClass()]
+    public class OrdenacaoTests
+    {
+        [TestMethod()]
+        public void bubbleSortTest()
+        {
+            int[] vetor = { 4, 6, 2, 7, 1 };
+            int [] esperado = { 1, 2, 4, 6, 7 };
+
+            var ordenacao = new Ordenacao();
+            int [] resultado = ordenacao.bubbleSort(vetor);
+            Assert.AreEqual(esperado, resultado);
+        }
+    }
+}
