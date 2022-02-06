f13822509092a591a418795531c0525389973abc
Beatriz Assumção da Silva <beatriz.assumcao@unifesp.br>
2022-02-06 16:31:30 -0300

    Adicionando Web API

--- Cadastro de series com c#/Series/Classes/Serie.cs
+++ Cadastro de series com c#/Series/Classes/Serie.cs
@@ -3,11 +3,11 @@ namespace Series
 {
     public class Serie : EntidadeBase
     {
-      private Genero Genero { get; set; }
+		private Genero Genero { get; set; }
 		private string Titulo { get; set; }
 		private string Descricao { get; set; }
 		private int Ano { get; set; }
-      private bool Excluido {get; set;}
+		private bool Excluido {get; set;}
 		private int Temporadas {get; set;}
 		private int avaliacao {get; set;}
 
@@ -19,7 +19,7 @@ namespace Series
 			this.Descricao = descricao;
 			this.Ano = ano;
 			this.Temporadas = temporadas;
-         this.Excluido = false;
+			this.Excluido = false;
 			this.avaliacao = avaliacao;
 		}
 
@@ -30,8 +30,8 @@ namespace Series
             retorno += "Titulo: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
             retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
-				retorno += "Quantidade de temporadas: " + this.Temporadas + Environment.NewLine;
-				retorno += "Avaliacao: " + this.avaliacao + Environment.NewLine;
+			retorno += "Quantidade de temporadas: " + this.Temporadas + Environment.NewLine;
+			retorno += "Avaliacao: " + this.avaliacao + Environment.NewLine;
             retorno += "Excluido: " + this.Excluido;
 			return retorno;
 		}
@@ -51,6 +51,30 @@ namespace Series
 			return this.Excluido;
 		}
 
+		public Genero getGenero()
+        {
+			return this.Genero;
+        }
+
+		public string getDescricao()
+        {
+			return this.Descricao;
+        }
+
+		public int getAno()
+		{
+			return this.Ano;
+		}
+
+		public int getTemporadas()
+		{
+			return this.Temporadas;
+		}
+
+		public int getAvalicao()
+		{
+			return this.avaliacao;
+		}
 
 		public void Excluir() {
             this.Excluido = true;
